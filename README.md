8d61aa1faf175b0560a4e8e21b85b50a51617ab0
Ryan Gao <ryantchgao@googlemail.com>
2022-08-25 16:39:51 -0700

    update gitignore and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # personal website
+
+Technologies: HTML, SCSS, JavaScript, React.js, Redux, Firebase
+
+first iteration of my personal website, powered by React.js
--- src/Pages/ProjectsPage.js
+++ src/Pages/ProjectsPage.js
@@ -30,7 +30,7 @@ const projectInformation = [
         title: "Personal Website",
         github: "https://github.com/toblrne/personalwebsite",
         technologies: ["HTML", "SCSS", "JavaScript", "React.js", "Redux", "Firebase"],
-        description: "First iteration of my personal website. Implemented a light-dark mode toggle using Redux. Deployed using Firebase. ",
+        description: "First iteration of my personal website. Implemented a light-dark mode toggle using Redux Toolkit. Deployed using Firebase. ",
         demo: "https://ryangao.dev"
 
     },
